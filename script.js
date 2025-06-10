f53e9c408851d377f3a545628ebe8f190aca6348
alepar <76537532+alepar666@users.noreply.github.com>
2025-06-10 14:02:36 +0200

    first integration
    player now is through howler.js

--- script.js
+++ script.js
@@ -46,21 +46,33 @@ var currentNowPlayingUrl;
 var selectedChannel;
 var nowPlayingRequestTimer;
 var channelContentUrl;
+var howlerPlayer;
 
 function playChannel(channelNumber) {
     reset();
     feedHTML(LOADING_DIV_ID, LOADING_MSG);
     var source = document.getElementById(AUDIO_PLAYER_SOURCE_ID);
     var channelElement = document.getElementById(channelsId[channelNumber - 1]);
-    source.src = channelElement.getAttribute(CHANNEL_DATA_VALUE_KEY);
-    audio.load();
-    audio.play();
+    //source.src = channelElement.getAttribute(CHANNEL_DATA_VALUE_KEY);
+
+    // audio.load();
+    // audio.play();
+    this.howlerPlayer = new Howl({
+        src: ['https://radio.intergalactic.fm/' + channelNumber],
+        html5: true,
+        format: ['mp3', 'aac']
+    });
+    howlerPlayer.play();
+    feedHTML(LOADING_DIV_ID, EMPTY_VAL);
+
     currentNowPlayingUrl = NOW_PLAYING_REQUEST_PREFIX + channelElement.innerHTML;
     channelContentUrl = NOW_PLAYING_PICTURE_REQUEST_PREFIX + channelNumber;
     selectedChannel = channelNumber;
     document.getElementById(channelNumber + ONGOING).classList.add(ONGOING);
+    getNowPlaying(currentNowPlayingUrl);
 }
 
+/*
 // when the audio player has finished loading and is ready to play
 audio.addEventListener(AUDIO_EVENT_PLAYING_NAME, function () {
     feedHTML(LOADING_DIV_ID, EMPTY_VAL);
@@ -81,6 +93,8 @@ audio.addEventListener(AUDIO_EVENT_PAUSE_NAME, function (e) {
     audio.currentTime = 0;
     reset();
 });
+
+*/
 
 // request now playing from IFM server every NOW_PLAYING_REQUEST_TIMEOUT_MSEC
 var previousTrackTitle = EMPTY_VAL;
@@ -132,8 +146,12 @@ function feedNowPlaying(value) {
 }
 
 function reset() {
+    if (this.howlerPlayer) {
+        this.howlerPlayer.stop();
+    }
+
     clearTimeout(nowPlayingRequestTimer);
-    audio.controls = EMPTY_VAL;
+    //audio.controls = EMPTY_VAL;
     removeOngoingMarker();
     removeWebConnectorDependencies();
     feedHTML(NOW_PLAYING_DIV_ID, EMPTY_VAL);
@@ -194,14 +212,14 @@ async function extractCoverFromChannelContent() {
 https://github.com/web-scrobbler/web-scrobbler/blob/master/src/connectors/intergalacticfm.ts#L8
 */
 function removeWebConnectorDependencies() {
-    audio.classList.remove(VJS_PLAY_CONTROL_CLASS);
-    audio.classList.remove(VJS_PLAYING_CLASS);
+    //audio.classList.remove(VJS_PLAY_CONTROL_CLASS);
+    //audio.classList.remove(VJS_PLAYING_CLASS);
     document.getElementById(TRACK_META_DIV_ID).classList.remove(TRACK_META_CLASS);
 }
 
 function addWebConnectorDependencies() {
-    audio.classList.add(VJS_PLAY_CONTROL_CLASS);
-    audio.classList.add(VJS_PLAYING_CLASS);
+    //audio.classList.add(VJS_PLAY_CONTROL_CLASS);
+    //audio.classList.add(VJS_PLAYING_CLASS);
     document.getElementById(TRACK_META_DIV_ID).classList.add(TRACK_META_CLASS);
 }
 
